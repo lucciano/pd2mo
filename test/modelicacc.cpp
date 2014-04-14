0da79d6c6fbbb54702b67af8174ceff7e96966ea
Luciano Andrade <andrade.luciano@gmail.com>
2014-04-13 21:33:20 -0300

    +1 test

--- test/modelicacc.cpp
+++ test/modelicacc.cpp
@@ -6,12 +6,18 @@
 #include <ast/stored_definition.h>
 #include "parser/parse.h"
 
+using namespace std;
 
 BOOST_AUTO_TEST_CASE( cero ) {
     BOOST_CHECK( 1 == 1 );
     int r = 0;
     string path = getFullPath();
     string filename = path + "/data/Constant.mo";
+    cout << filename << endl;
     AST_StoredDefinition sd = parseFile(filename,&r);
     AST_ClassList models = sd->models();
+    AST_ClassListIterator mIter;
+    for(mIter = models->begin(); mIter != models->end(); ++mIter){
+	BOOST_CHECK( 0 == (*mIter)->name()->compare("Constant"));
+    }
 }
