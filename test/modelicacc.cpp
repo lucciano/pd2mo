3e68dcd800b690150906a193b240e4bcbc51a38a
Luciano Andrade <andrade.luciano@gmail.com>
2014-04-16 18:40:44 -0300

    esqueleto de traverser

--- test/modelicacc.cpp
+++ test/modelicacc.cpp
@@ -4,7 +4,9 @@
 #include <test/file_util.h>
 #include <string>
 #include <ast/stored_definition.h>
-#include "parser/parse.h"
+#include <parser/parse.h>
+#include <util/symbol_table.h>
+#include <util/ast_util.h>
 
 using namespace std;
 
@@ -18,6 +20,87 @@ BOOST_AUTO_TEST_CASE( cero ) {
     AST_ClassList models = sd->models();
     AST_ClassListIterator mIter;
     for(mIter = models->begin(); mIter != models->end(); ++mIter){
-	BOOST_CHECK( 0 == (*mIter)->name()->compare("Constant"));
+	AST_Class c = (*mIter);
+	BOOST_CHECK( 0 == c->name()->compare("Constant"));
+
+	cout << "Encontramos la clase " << c->name() << endl;
+	for( AST_ElementListIterator cIter = c->composition()->elementList()->begin();
+		cIter != c->composition()->elementList()->end(); ++cIter){
+		//cout << (*cIter) << endl;
+	}
+	AST_CompositionElementList cl = c->composition()->compositionList();
+	AST_CompositionElementListIterator it;
+	foreach(it, cl){
+		AST_EquationList eql = (*it)->getEquationsAlgs()->getEquations();
+		AST_EquationListIterator it2;
+		foreach(it2, eql){
+			cout << (*it2) << endl;
+		}
+	}
     }
+}
+
+BOOST_AUTO_TEST_CASE( uno) {
+    string path = getFullPath();
+    string filename = path + "/data/Model01.mo";
+
+    int r = 0;
+    AST_StoredDefinition sd = parseFile(filename,&r);
+    
+    cout << filename << endl;
+
+    AST_ClassList models = sd->models();
+    AST_ClassListIterator mIter;
+    for(mIter = models->begin(); mIter != models->end(); ++mIter){
+	AST_Class c = (*mIter);
+	
+	AST_Composition comp = c->composition();
+
+	// First elements
+	AST_ElementList el = comp->elementList();
+	AST_ElementListIterator elit;
+	foreach(elit,el) {
+		//switch (current_element(elit)->elementType()) {
+		//	case COMPONENT:
+		//		addVariable(current_element(elit)->getAsComponent());
+		//		break;
+		//	default:
+		//		addElement(current_element(elit));
+		//		break;
+		//}
+	}
+	
+	
+	AST_CompositionElementList cel = comp->compositionList();
+	AST_CompositionElementListIterator it;
+	
+	foreach(it,cel) {
+		AST_EquationListIterator eqit;
+		AST_ElementListIterator  elit;
+		AST_StatementListIterator stit;
+		
+		// Equations 
+		AST_CompositionElement e = current_element(it);
+		AST_CompositionEqsAlgs eqA = e->getEquationsAlgs();
+		if (eqA != NULL) {
+			if  (eqA->isInitial()) {
+				foreach(eqit,eqA->getEquations())  
+					cout << current_element(eqit) << endl;
+				foreach(stit,eqA->getAlgorithms()) 
+					cout << current_element(stit) << endl;
+			} else {
+				foreach(eqit,eqA->getEquations())  
+					cout << current_element(eqit) << endl;
+				foreach(stit,eqA->getAlgorithms()) 
+					cout << current_element(stit) << endl;
+			}
+		}
+		// Elements 
+		foreach(elit,e->getElementList()) {
+			//AST_ListAppend(_comps , current_element(elit)->getAsComponent() );
+			cout << current_element(elit)->getAsComponent() << endl;
+		}
+	}	
+	//_ct = new TypeCheck_( tyEnv , varEnv );
+	}
 }
