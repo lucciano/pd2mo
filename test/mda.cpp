5e5e2be9830861f8b4594fc3a1d222f49b2aba8c
Luciano Andrade <andrade.luciano@gmail.com>
2014-11-14 19:04:58 -0300

    sample of eval

--- test/mda.cpp
+++ test/mda.cpp
@@ -28,11 +28,25 @@ BOOST_AUTO_TEST_CASE( multidimarray){
 }
 
 BOOST_AUTO_TEST_CASE( mmo ){
-    string filename = "/home/powerdevs/pd2mo/test/data/eval.mo";
+    string filename = "/home/powerdevs/pd2mo/test/data/EvalCondition.mo";
     int r = 0;
     TypeSymbolTable tyEnv = newTypeSymbolTable();
     AST_Class ast_c = parseClass(filename,&r);
 
     MMO_Class c = newMMO_Class(ast_c, tyEnv);
 	cout << c ; 
+  AST_Expression_Integer intExp =  (AST_Expression_Integer) newAST_Expression_Real(0.0);
+  //AST_Expression_Integer intExp2 = (AST_Expression_Integer) newAST_Expression_Integer(2);
+  AST_Expression_ComponentReference intExp2 = (AST_Expression_ComponentReference) newAST_Expression_ComponentReference();
+  AST_String s = new std::string("shift");
+  intExp2->append(s, new std::list<AST_Expression>());
+  AST_Expression_BinOp binOp = (AST_Expression_BinOp) newAST_Expression_BinOp(intExp2, intExp, BINOPGREATER);
+
+  EvalExp *evalExp = new EvalExp(c->getVarSymbolTable());
+  AST_Expression result = evalExp->eval(binOp);
+	cout << result << endl;
+	cout << evalExp->eval(intExp2) << endl;
+	cout << intExp << endl;
+	cout << binOp << endl;
+
 }
